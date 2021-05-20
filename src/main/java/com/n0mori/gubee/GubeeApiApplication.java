b9eb4b2a1a869cf334c5d42a09e9646e177eed2d
omori <omori@uel.br>
2021-05-20 20:33:36 -0300

    Initial commit

---
+++ src/main/java/com/n0mori/gubee/GubeeApiApplication.java
@@ -0,0 +1,13 @@
+package com.n0mori.gubee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GubeeApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GubeeApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/n0mori/gubee/TesteGubeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n0mori.gubee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteGubeeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
